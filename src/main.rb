3a78b071cc211faeaa762aa8fc6032e26ef9b9db
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-14 20:50:48 +1100

    passed potfolio_assets_quantities_array to show_portfolio()

--- src/main.rb
+++ src/main.rb
@@ -24,7 +24,7 @@ while logged_in
     while active_selection[1] == "fusion22" # change this to use user object attr subclass of user
         logged_in_admin_main_menu() # Optionally pass a title to the Main Menu
         user_selection = gets.strip.chomp.to_i # get user selection
-        active_selection = admin_logged_in_menu_selection(user_selection, path_to_users_file, path_to_portfolio_file)
+        active_selection = admin_logged_in_menu_selection(user_selection, path_to_users_file, path_to_portfolio_file) # array is returned with true/false for logged in at index 0, and a custom value for each action chosen at index 1
         active_selection[0] == false ? exit : logged_in = active_selection[0] # exits or sets logged in status to true
     end
     while active_selection[1] != "fusion22" # change this to use user object attr
@@ -34,8 +34,8 @@ while logged_in
         active_selection[0] == false ? exit : logged_in = active_selection[0] # exits or sets logged in status to true
         if active_selection[1] == "show_portfolio"
             # show portfolio code
-            puts read_portfolio_json(path_to_portfolio_file)
-            # show_portfolio()
+            portfolio_assets_quantities_array = read_portfolio_json(path_to_portfolio_file)
+            show_portfolio(portfolio_assets_quantities_array)
             # end show portfolio code
         end
     end
