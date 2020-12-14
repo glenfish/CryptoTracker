c9e0bec857ebec1def1365a454581e366006c669
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-14 17:26:54 +1100

    added methods to handle logged in menu display

--- src/main.rb
+++ src/main.rb
@@ -5,13 +5,14 @@ require 'json'
 require 'terminal-table'
 require 'colorize'
 
+
 require_relative 'methods'
-require_relative 'paths'
+
 
 path_to_users_file = './json/users/users.json'
-# users_json = get_user(path_to_users_file)
+path_to_portfolio_file = "./json/portfolios/glenfish.json"
 
-# FINISHED CODE
+# START CODE
 logged_in = false
 while !logged_in
     top_level_menu("Crypto Portfolio Tracker - Logged Out") # Optionally pass a title to the Main Menu
@@ -20,10 +21,20 @@ while !logged_in
     active_selection[0] == false ? exit : logged_in = active_selection[0] # exits or sets logged in status to true
 end
 while logged_in
-    logged_in_main_menu("Welcome to Crypto Portfolio Tracker") # Optionally pass a title to the Main Menu
-    user_selection = gets.strip.chomp.to_i # get user selection
+    while active_selection[1] == "fusion22" # change this to use user object attr subclass of user
+        logged_in_admin_main_menu() # Optionally pass a title to the Main Menu
+        user_selection = gets.strip.chomp.to_i # get user selection
+        active_selection = admin_logged_in_menu_selection(user_selection, path_to_users_file, path_to_portfolio_file)
+        active_selection[0] == false ? exit : logged_in = active_selection[0] # exits or sets logged in status to true
+    end
+    while active_selection[1] != "fusion22" # change this to use user object attr
+        logged_in_main_menu("Welcome to Crypto Portfolio Tracker") # Optionally pass a title to the Main Menu
+        user_selection = gets.strip.chomp.to_i # get user selection
+        active_selection = logged_in_menu_selection(user_selection, path_to_users_file, path_to_portfolio_file)
+        active_selection[0] == false ? exit : logged_in = active_selection[0] # exits or sets logged in status to true
+    end
 end
-# END FINISHED CODE
+# END CODE
 
 
 
