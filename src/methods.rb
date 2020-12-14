697162631c2fa5cfb7f8cc1d3951b1bbd2acea9a
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-14 21:56:16 +1100

    added asset filtering against api and quantity, total price

--- src/methods.rb
+++ src/methods.rb
@@ -173,64 +173,83 @@ def admin_logged_in_menu_selection(selection, path_to_users_file, path_to_portfo
     end
 end
 
-# def show_portfolio
-#     system 'clear'
-#     puts "which test file... 1, 2, 3 or 4?\n"
-# choice = gets.chomp.to_i
-# case choice
-#     when 1
-#     api_test_file = './json/api_cached/temp-1.json'
-#     when 2
-#     api_test_file = './json/api_cached/temp-2.json'
-#     when 3
-#     api_test_file = './json/api_cached/temp-3.json'
-#     when 4
-#     api_test_file = './json/api_cached/temp-4.json'
-#   else
-#     api_test_file = './json/api_cached/schema.json'
-# end
+def show_portfolio(portfolio_assets_quantities_array)
+    system 'clear'
+    puts "which test file... 1, 2, 3 or 4?\n"
+choice = gets.chomp.to_i
+case choice
+    when 1
+    api_test_file = './json/api_cached/temp-1.json'
+    when 2
+    api_test_file = './json/api_cached/temp-2.json'
+    when 3
+    api_test_file = './json/api_cached/temp-3.json'
+    when 4
+    api_test_file = './json/api_cached/temp-4.json'
+  else
+    api_test_file = './json/api_cached/schema.json'
+end
 
-# puts "big list... y or n ?\n"
-# if gets.chomp == "y"
-#     portfolio_array = %w[BTC ETH XRP USDT BCH LTC LINK ADA DOT BNB XLM USDC BSV EOS XMR WBTC TRX XEM XTZ LEO FIL CRO NEO DAI VET REV ATOM AAVE DASH WAVES HT MIOTA UNI ZEC ETC YFI THETA BUSD COMP CEL MKR SNX OMG DOGE UMA KSM FTT ONT ZIL ALGO SUSHI OKB BTT BAT TUSD RENBTC DCR NEXO ZRX DGB PAX HUSD AVAX REN QTUM HBAR AMPL ICX ABBC CELO LRC EGLD HEDG STX LUNA KNC RSR REP EWT LSK OCEAN BTG SC QNT RUNE CVT NANO BAND MANA ZB NMR ENJ ANT MAID SNT CHSB XVG NXM RVN]
-# else
-#     portfolio_array = %w[BTC ETH XRP USDT BCH LTC LINK ADA]
-# end
+puts "big list... y or n ?\n"
+if gets.chomp == "y"
+    portfolio_array = %w[BTC ETH XRP USDT BCH LTC LINK ADA DOT BNB XLM USDC BSV EOS XMR WBTC TRX XEM XTZ LEO FIL CRO NEO DAI VET REV ATOM AAVE DASH WAVES HT MIOTA UNI ZEC ETC YFI THETA BUSD COMP CEL MKR SNX OMG DOGE UMA KSM FTT ONT ZIL ALGO SUSHI OKB BTT BAT TUSD RENBTC DCR NEXO ZRX DGB PAX HUSD AVAX REN QTUM HBAR AMPL ICX ABBC CELO LRC EGLD HEDG STX LUNA KNC RSR REP EWT LSK OCEAN BTG SC QNT RUNE CVT NANO BAND MANA ZB NMR ENJ ANT MAID SNT CHSB XVG NXM RVN]
+else
+    portfolio_array = %w[BTC ETH XRP USDT BCH LTC LINK ADA]
+end
+p portfolio_assets_quantities_array
+output = []
+portfolio_assets_quantities_array.each do |element|
+    symbol = element[0]
+    portfolio_array.map do |crypto|
+    output << crypto if crypto == symbol
+    end
+end
+portfolio_array = output
+p portfolio_array
+
 # system 'clear'
-# portfolio = portfolio_array.join(',')
-# api_link = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=' + portfolio
-# api_key = '' # user enters this at run time for security
-# def get_crypto(response, portfolio_array)
-#   rows =[]
-#   portfolio_array.each do |crypto|
-#     name = response['data'][crypto]['name']
-#     symbol = response['data'][crypto]['symbol']
-#     price = format('%0.2f', response['data'][crypto]['quote']['USD']['price']).gsub(/(\d)(?=\d{3}+\.)/, '\1,')
-#     rows << [name, symbol, "$" + price]
-#   end
+portfolio = portfolio_array.join(',')
+api_link = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=' + portfolio
+api_key = '' # user enters this at run time for security
+def get_crypto(response, portfolio_array, portfolio_assets_quantities_array)
+  rows =[]
+  portfolio_array.each do |crypto|
+    name = response['data'][crypto]['name']
+    symbol = response['data'][crypto]['symbol']
+    price = format('%0.2f', response['data'][crypto]['quote']['USD']['price']).gsub(/(\d)(?=\d{3}+\.)/, '\1,')
+    quantity = 0
+    portfolio_assets_quantities_array.each do |element|
+        p "symbol: #{element[0]} qty: #{element[1]}"
+        if symbol == element[0]
+            quantity = element[1]
+        end
+    end
+    total = format('%0.2f', quantity * response['data'][crypto]['quote']['USD']['price']).gsub(/(\d)(?=\d{3}+\.)/, '\1,')
+    rows << [name, symbol, quantity, "$" + price, "$" + total]
+  end
 
-#   table = Terminal::Table.new :title => "Portfolio".colorize(:cyan), :headings => ['Name'.colorize(:cyan), 'Symbol'.colorize(:cyan), 'Price USD'.colorize(:cyan)], :rows => rows
-#   puts table
-#   puts "\nYour portfolio has a total of #{portfolio_array.length + 1} cryptos\n"
-# end
+  table = Terminal::Table.new :title => "Portfolio".colorize(:cyan), :headings => ['Name'.colorize(:cyan), 'Symbol'.colorize(:cyan), 'Quantity'.colorize(:cyan), 'Price USD'.colorize(:cyan), 'Total USD'.colorize(:cyan)], :rows => rows
+  puts table
+  puts "\nYour portfolio has a total of #{portfolio_array.length + 1} cryptos\n"
+end
 
-# def call_api(api_link)
-#   response = HTTParty.get(api_link,
-#                           { headers: { 'X-CMC_PRO_API_KEY' => api_key,
-#                                        'Accept' => 'application/json' } })
-#   JSON.parse(response.body)
-# end
+def call_api(api_link)
+  response = HTTParty.get(api_link,
+                          { headers: { 'X-CMC_PRO_API_KEY' => api_key,
+                                       'Accept' => 'application/json' } })
+  JSON.parse(response.body)
+end
 
-# def call_dummy_api(api_test_file)
-#     file = File.open(api_test_file)
-#     file_data = file.read
-#     return JSON.parse(file_data)
-# end
+def call_dummy_api(api_test_file)
+    file = File.open(api_test_file)
+    file_data = file.read
+    return JSON.parse(file_data)
+end
 
-# dummy_response = call_dummy_api(api_test_file)
+dummy_response = call_dummy_api(api_test_file)
 
-# get_crypto(dummy_response, portfolio_array)
-# end
+get_crypto(dummy_response, portfolio_array, portfolio_assets_quantities_array)
+end
 
 # method to read portfolio json
 def read_portfolio_json(path_to_portfolio_file)
