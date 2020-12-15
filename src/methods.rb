01691dd6cebb1b2ff38fc1c477c7c3b32c46a342
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-15 13:12:08 +1100

    cleanup and added error handling for main menu

--- src/methods.rb
+++ src/methods.rb
@@ -163,6 +163,7 @@ def top_level_menu_selection(selection, path_to_users_file)
 end
 
 def logged_in_menu_selection(selection, path_to_users_file, path_to_portfolio_file)
+    system 'clear'
     case selection
     when 1
         # help
@@ -178,8 +179,7 @@ def logged_in_menu_selection(selection, path_to_users_file, path_to_portfolio_fi
         # quit
         return [false, "exit"]
     else
-        # error
-        raise AppError
+        return [true, "error"]
     end
 end
 
@@ -208,121 +208,112 @@ def show_portfolio(portfolio_assets_quantities_array)
     system 'clear'
 
 
-# puts "big list... y or n ?\n"
-# if gets.chomp == "y"
+    # puts "big list... y or n ?\n"
+    # if gets.chomp == "y"
     portfolio_array = %w[BTC ETH XRP USDT BCH LTC LINK ADA DOT BNB XLM USDC BSV EOS XMR WBTC TRX XEM XTZ LEO FIL CRO NEO DAI VET REV ATOM AAVE DASH WAVES HT MIOTA UNI ZEC ETC YFI THETA BUSD COMP CEL MKR SNX OMG DOGE UMA KSM FTT ONT ZIL ALGO SUSHI OKB BTT BAT TUSD RENBTC DCR NEXO ZRX DGB PAX HUSD AVAX REN QTUM HBAR AMPL ICX ABBC CELO LRC EGLD HEDG STX LUNA KNC RSR REP EWT LSK OCEAN BTG SC QNT RUNE CVT NANO BAND MANA ZB NMR ENJ ANT MAID SNT CHSB XVG NXM RVN]
-# else
-    # portfolio_array = %w[BTC ETH XRP USDT BCH LTC LINK ADA]
-# end
-# p portfolio_assets_quantities_array
-output = []
-portfolio_assets_quantities_array.each do |element|
-    symbol = element[0]
-    portfolio_array.map do |crypto|
-    output << crypto if crypto == symbol
-    end
-end
-portfolio_array = output
-# p portfolio_array
-
-# system 'clear'
-portfolio = portfolio_array.join(',')
-api_link = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=' + portfolio
-api_key = '' # user enters this at run time for security
-def get_crypto(response, portfolio_array, portfolio_assets_quantities_array)
-  rows =[]
-  grand_total = 0.0
-  portfolio_array.each do |crypto|
-    name = response['data'][crypto]['name']
-    symbol = response['data'][crypto]['symbol']
-    price = format('%0.2f', response['data'][crypto]['quote']['USD']['price']).gsub(/(\d)(?=\d{3}+\.)/, '\1,')
-    quantity = 0
+    # else
+        # portfolio_array = %w[BTC ETH XRP USDT BCH LTC LINK ADA]
+    # end
+    # p portfolio_assets_quantities_array
+    output = []
     portfolio_assets_quantities_array.each do |element|
-        # p "symbol: #{element[0]} qty: #{element[1]}"
-        if symbol == element[0]
-            quantity = element[1]
+        symbol = element[0]
+        portfolio_array.map do |crypto|
+        output << crypto if crypto == symbol
         end
     end
-    grand_total += quantity * response['data'][crypto]['quote']['USD']['price']
-    total = format('%0.2f', quantity * response['data'][crypto]['quote']['USD']['price']).gsub(/(\d)(?=\d{3}+\.)/, '\1,')
-    rows << [name, symbol, quantity, "$" + price, "$" + total]
-  end
-  rows << :separator
-  grand_total = format('%0.2f', grand_total).gsub(/(\d)(?=\d{3}+\.)/, '\1,')
-  rows << ['Grand Total','','','','$' + grand_total]
-  table = Terminal::Table.new :title => "Portfolio".colorize(:cyan), :headings => ['Name'.colorize(:cyan), 'Symbol'.colorize(:cyan), 'Quantity'.colorize(:cyan), 'Price USD'.colorize(:cyan), 'Total USD'.colorize(:cyan)], :rows => rows
-  puts table
-  puts "\nYour portfolio has a total of #{portfolio_array.length} digital assets.\n"
-end
+    portfolio_array = output
 
-def call_api(api_link, api_key, filepath = "./json/api_cached/latest.json")
-  response = HTTParty.get(api_link,
-                          { headers: { 'X-CMC_PRO_API_KEY' => api_key,
-                                       'Accept' => 'application/json' } })
-  parsed = JSON.parse(response.body)
-  write_json_file(parsed, filepath)
-  return parsed
-end
-
-def call_dummy_api(api_test_file)
-    file = File.open(api_test_file)
-    file_data = file.read
-    return JSON.parse(file_data)
-end
-
-
-
-
-puts "1. Use Locally Cached API Test Data\n2. Get Live CoinMarketCap.com API Data\n"
-api_course_selection = gets.strip.chomp.to_i
-case api_course_selection
-when 1
-    begin
-    puts "Select an API test file... 1/2/3/4:\nOr select 5 for the most recent cached API file (for testing/demo)\nNOTE: If portfolio has changed since last cached, run live api call.)\n"
-    choice = gets.chomp.to_i
-    case choice
-        when 1
-        api_test_file = './json/api_cached/temp-1.json'
-        when 2
-        api_test_file = './json/api_cached/temp-2.json'
-        when 3
-        api_test_file = './json/api_cached/temp-3.json'
-        when 4
-        api_test_file = './json/api_cached/temp-4.json'
-        when 5
-        api_test_file = './json/api_cached/latest.json'
-      else
-        api_test_file = './json/api_cached/schema.json'
+    portfolio = portfolio_array.join(',')
+    api_link = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=' + portfolio
+    api_key = '' # user enters this at run time for security
+    def get_crypto(response, portfolio_array, portfolio_assets_quantities_array)
+    rows =[]
+    grand_total = 0.0
+    portfolio_array.each do |crypto|
+        name = response['data'][crypto]['name']
+        symbol = response['data'][crypto]['symbol']
+        price = format('%0.2f', response['data'][crypto]['quote']['USD']['price']).gsub(/(\d)(?=\d{3}+\.)/, '\1,')
+        quantity = 0
+        portfolio_assets_quantities_array.each do |element|
+            # p "symbol: #{element[0]} qty: #{element[1]}"
+            if symbol == element[0]
+                quantity = element[1]
+            end
+        end
+        grand_total += quantity * response['data'][crypto]['quote']['USD']['price']
+        total = format('%0.2f', quantity * response['data'][crypto]['quote']['USD']['price']).gsub(/(\d)(?=\d{3}+\.)/, '\1,')
+        rows << [name, symbol, quantity, "$" + price, "$" + total]
     end
-    rescue
-    puts "Please enter a number between 1 and 4"
-    retry
+    rows << :separator
+    grand_total = format('%0.2f', grand_total).gsub(/(\d)(?=\d{3}+\.)/, '\1,')
+    rows << ['Grand Total','','','','$' + grand_total]
+    table = Terminal::Table.new :title => "Portfolio".colorize(:cyan), :headings => ['Name'.colorize(:cyan), 'Symbol'.colorize(:cyan), 'Quantity'.colorize(:cyan), 'Price USD'.colorize(:cyan), 'Total USD'.colorize(:cyan)], :rows => rows
+    puts table
+    puts "\nYour portfolio has a total of #{portfolio_array.length} digital assets.\n"
     end
-    begin
-    system 'clear'
-    dummy_response = call_dummy_api(api_test_file) # cached local call
-    get_crypto(dummy_response, portfolio_array, portfolio_assets_quantities_array)
-    rescue
-    puts "Your portfolio has changed. Run a fresh API call to get the latest data"
 
-end
-    
-when 2
-    begin
-    puts "Enter API key:\n"
-    api_key = gets.strip.chomp
-    system 'clear'
-    puts "*** ... loading live data ... ***\n\n"
-    response = call_api(api_link, api_key) # live call
-    get_crypto(response, portfolio_array, portfolio_assets_quantities_array)
-    rescue
-        puts "Error: api key does not match"
+    def call_api(api_link, api_key, filepath = "./json/api_cached/latest.json")
+    response = HTTParty.get(api_link,
+                            { headers: { 'X-CMC_PRO_API_KEY' => api_key,
+                                        'Accept' => 'application/json' } })
+    parsed = JSON.parse(response.body)
+    write_json_file(parsed, filepath)
+    return parsed
     end
-end
 
-
-
-end
+    def call_dummy_api(api_test_file)
+        file = File.open(api_test_file)
+        file_data = file.read
+        return JSON.parse(file_data)
+    end
+    # handles the I/O of selecting the portfolio file source
+    puts "1. Use Locally Cached API Test Data\n2. Get Live CoinMarketCap.com API Data\n"
+    api_course_selection = gets.strip.chomp.to_i
+    case api_course_selection
+    when 1
+        begin
+        puts "Select an API test file... 1/2/3/4:\nOr select 5 for the most recent cached API file (for testing/demo)\nNOTE: If portfolio has changed since last cached, run live api call.)\n"
+        choice = gets.chomp.to_i
+        case choice
+            when 1
+            api_test_file = './json/api_cached/temp-1.json'
+            when 2
+            api_test_file = './json/api_cached/temp-2.json'
+            when 3
+            api_test_file = './json/api_cached/temp-3.json'
+            when 4
+            api_test_file = './json/api_cached/temp-4.json'
+            when 5
+            api_test_file = './json/api_cached/latest.json'
+        else
+            api_test_file = './json/api_cached/schema.json'
+        end
+        rescue
+        puts "Please enter a number between 1 and 4"
+        retry
+        end
+        begin
+        system 'clear'
+        dummy_response = call_dummy_api(api_test_file) # cached local call
+        get_crypto(dummy_response, portfolio_array, portfolio_assets_quantities_array)
+        rescue
+        puts "Your portfolio has changed. Run a fresh API call to get the latest data"
+    end
+        
+    when 2
+        begin
+        puts "Enter API key:\n"
+        api_key = gets.strip.chomp
+        system 'clear'
+        puts "*** ... loading live data ... ***\n\n"
+        response = call_api(api_link, api_key) # live call
+        get_crypto(response, portfolio_array, portfolio_assets_quantities_array)
+        rescue
+            puts "Error: api key does not match"
+        end
+    end
+end # end show_portfolio method
 
 # method to read portfolio json
 def read_portfolio_json(path_to_portfolio_file)
@@ -342,7 +333,4 @@ def read_portfolio_json(path_to_portfolio_file)
     # p portfolio_array
     # puts "\n"
     return portfolio_array
-
-
 end
-
