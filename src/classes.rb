286c7ea1efa98ab88cca4a6cbe61a2bd90b10e79
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-15 22:18:28 +1100

    added Portfolio Class

--- src/classes.rb
+++ src/classes.rb
@@ -18,5 +18,29 @@ class Users
         return "Name: #{@name} Username: #{@username} Password: #{@password} Active: #{@active} Created: #{@user_created}"
     end
     
+end
+
+class Portfolio
+    attr_reader
+    attr_writer
+    attr_accessor
+
+    def initialize(username)
+        @username = username
+    end
+
+    # methods
+    def to_s
+        return @username
+    end
+
+    def portfolio_path
+        return "./json/portfolios/#{@username}.json"
+    end
+
+    def create_empty_portfolio
+        code = "{\"username\":\"#{@username}\",\"data\":{\"BTC\":{\"asset_name\":\"\",\"asset_quantity\":9.5,\"asset_buy_date\":\"2020-12-14\",\"asset_sell_date\":\"\",\"usd_price\":\"\",\"btc_price\":\"\",\"usd_profit\":\"\",\"btc_profit\":\"\"}}}"
+        return code
+    end
 
 end
