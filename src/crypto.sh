c57c65acd1088e0bc2610b10d079cbde3231ec60
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-17 16:48:29 +1100

    added flag passing to bash script

--- src/crypto.sh
+++ src/crypto.sh
@@ -1,2 +1,28 @@
 #! /bin/bash
-ruby ./main.rb
+# This will get arguments for flags
+# while getopts h:a:f: flag
+# do
+#     case "${flag}" in
+#         h) help=${OPTARG};;
+#         a) age=${OPTARG};;
+#         f) fullname=${OPTARG};;
+#     esac
+# done
+# echo "help: $help";
+# echo "Age: $age";
+# echo "Full Name: $fullname";
+
+# This will pass just flags
+i=1;
+output='';
+for flag in "$@" 
+do
+    output+=" "$flag;
+    i=$((i + 1));
+done
+# echo $output;
+
+# call ruby file
+ruby ./main.rb$output
+
+
