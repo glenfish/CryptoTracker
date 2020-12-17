fc27f290c4792f8b199f012087e9e1101e3cd6ae
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-17 12:39:32 +1100

    changed require_relative path and name, changed active_selection to selection

--- src/main.rb
+++ src/main.rb
@@ -4,7 +4,7 @@ require 'httparty'
 require 'json'
 require 'terminal-table'
 require 'colorize'
-require_relative 'methods'
+require_relative './methods/menu'
 path_to_users_file = './json/users/users.json'
 path_to_portfolio_file = "./json/portfolios/glenfish.json"
 active_user = ""
@@ -14,37 +14,37 @@ while !logged_in
     begin
     top_level_menu("Crypto Portfolio Tracker - You Are Logged Out") # Optionally pass a title to the Main Menu
     user_selection = gets.strip.chomp.to_i # get user selection
-    active_selection = top_level_menu_selection(user_selection, path_to_users_file)
-    active_selection[0] == false ? exit : logged_in = active_selection[0] # exits or sets logged in status to true
-    active_user = active_selection[2] # passes back the active user object
+    select = top_level_menu_selection(user_selection, path_to_users_file)
+    select[0] == false ? exit : logged_in = select[0] # exits or sets logged in status to true
+    active_user = select[2] # passes back the active user object
     rescue
         retry
     end
 end
 while logged_in
-    while active_selection[1] == "fusion22" # admin user (hard coded for now for use with this one username)
+    while select[1] == "fusion22" # admin user (hard coded for now for use with this one username)
         logged_in_admin_main_menu() # Optionally pass a title to the Main Menu
         user_selection = gets.strip.chomp.to_i # get user selection
-        active_selection = admin_logged_in_menu_selection(user_selection, path_to_users_file, path_to_portfolio_file) # array is returned with true/false for logged in at index 0, and a custom value for each action chosen at index 1
-        active_selection[0] == false ? exit : logged_in = active_selection[0] # exits or sets logged in status to true
+        select = admin_logged_in_menu_selection(user_selection, path_to_users_file, path_to_portfolio_file) # array is returned with true/false for logged in at index 0, and a custom value for each action chosen at index 1
+        select[0] == false ? exit : logged_in = select[0] # exits or sets logged in status to true
     end
-    while active_selection[1] != "fusion22" # regular user, not admin user
+    while select[1] != "fusion22" # regular user, not admin user
         logged_in_main_menu("Crypto Portfolio Tracker Main Menu") # Optionally pass a title to the Main Menu
         user_selection = gets.strip.chomp.to_i # get user selection
         begin
-        active_selection = logged_in_menu_selection(user_selection, path_to_users_file, path_to_portfolio_file)
+        select = logged_in_menu_selection(user_selection, path_to_users_file, path_to_portfolio_file)
         rescue
             retry
         end
-        active_selection[0] == false ? exit : logged_in = active_selection[0] # exits or sets logged in status to true
-        if active_selection[1] == "show_portfolio"
+        select[0] == false ? exit : logged_in = select[0] # exits or sets logged in status to true
+        if select[1] == "show_portfolio"
             # show portfolio code
             portfolio_assets_quantities_array = read_portfolio_json(path_to_portfolio_file, active_user)
             show_portfolio(portfolio_assets_quantities_array, active_user)
             # end show portfolio code
-        elsif active_selection[1] == "add_crypto"
+        elsif select[1] == "add_crypto"
             add_crypto_to_portfolio(active_user)
-        elsif active_selection[1] == "show_help"
+        elsif select[1] == "show_help"
             show_help
         end
     end
