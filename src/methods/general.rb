040b8fc76604ac1af9299da187135aefaa889b0c
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-19 10:58:33 +1100

    moved general methods here

---
+++ src/methods/general.rb
@@ -0,0 +1,9 @@
+# clear terminal screen
+def clear
+    system 'clear'
+end
+
+def title
+    title = "  __                    ___                    \n /    _     _  |_  _     |   _  _   _ |   _  _ \n \\__ |  \\/ |_) |_ (_)    |  |  (_| (_ |( (- |  \n        /  |                                   "
+    puts title
+end
