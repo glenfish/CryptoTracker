744f13b275a7aa56b434bc778892d622695426a5
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-19 14:50:11 +1100

    cleaned up comments in shell script

--- src/crypto.sh
+++ src/crypto.sh
@@ -1,21 +1,5 @@
 #! /bin/bash
 
-
-# This will pass just flags
-# i=1;
-# output='';
-# for flag in "$@" 
-# do
-#     output+=" "$flag;
-#     i=$((i + 1));
-# done
-# echo $output;
-
-# call ruby file
-# ruby ./main.rb$output
-
-# This will get arguments for flags
-out='';
 while getopts u:p:h flag
 do
     case "${flag}" in
@@ -25,10 +9,7 @@ do
     esac
 
 done
-# echo "$u";
-# echo "$p";
-# echo "$h";
-# echo $h $u $p
+
 ruby ./main.rb $h $u $p
 
 
