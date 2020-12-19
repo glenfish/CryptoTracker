b2d5e391044983cfacda529ac0d2c2a7dc9d7131
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-19 13:29:30 +1100

    added username password argv passthrough

--- src/crypto.sh
+++ src/crypto.sh
@@ -1,28 +1,34 @@
 #! /bin/bash
-# This will get arguments for flags
-# while getopts h:a:f: flag
-# do
-#     case "${flag}" in
-#         h) help=${OPTARG};;
-#         a) age=${OPTARG};;
-#         f) fullname=${OPTARG};;
-#     esac
-# done
-# echo "help: $help";
-# echo "Age: $age";
-# echo "Full Name: $fullname";
+
 
 # This will pass just flags
-i=1;
-output='';
-for flag in "$@" 
-do
-    output+=" "$flag;
-    i=$((i + 1));
-done
+# i=1;
+# output='';
+# for flag in "$@" 
+# do
+#     output+=" "$flag;
+#     i=$((i + 1));
+# done
 # echo $output;
 
 # call ruby file
-ruby ./main.rb$output
+# ruby ./main.rb$output
+
+# This will get arguments for flags
+out='';
+while getopts u:p:h flag
+do
+    case "${flag}" in
+        u) u=${OPTARG};;
+        p) p=${OPTARG};;
+        h) h='-h';;
+    esac
+
+done
+# echo "$u";
+# echo "$p";
+# echo "$h";
+# echo $h $u $p
+ruby ./main.rb $h $u $p
 
 
