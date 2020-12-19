f52686393054e2bab33052bb43a536fd2098b518
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-19 13:29:06 +1100

    added update message to user

--- src/methods/portfolio.rb
+++ src/methods/portfolio.rb
@@ -35,6 +35,9 @@ def add_crypto_to_portfolio(active_user)
     if gets.strip.chomp.downcase == 'y'
         json= {"username":active_user.username,"data":portfolio_json['data'].merge({symbol=>{"asset_name"=>"", "asset_quantity"=>quantity, "asset_buy_date"=>Time.now.strftime("%Y-%m-%d"), "asset_sell_date"=>"", "usd_price"=>"", "btc_price"=>"", "usd_profit"=>"", "btc_profit"=>""}})}
         write_json_file(json, path_to_portfolio_file)
+        puts "Your portfolio was updated!"
+    else
+        puts "Your portfolio was not changed"
     end
 rescue
 
