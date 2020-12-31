6645febddd88296716004b6b59426ebc02be00bc
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2020-12-31 14:44:19 +1100

    rm rspec files

--- src/spec/main_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../main.rb'
-
-describe 'call_dummy_api' do
-  it 'should return JSON data' do
-    expect(call_dummy_api('./json/api_cached/temp-1.json')).to be_truthy
-    expect(call_dummy_api('./json/api_cached/temp-1.json')).to have_key("status")
-  end
-end
--- src/spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-config.include JSON::SchemaMatchers
-
-#schema file
-config.json_schemas[:my_schema] = "../json/api_cached/schema.json"
-#inline
-config.json_schemas[:inline_schema] = '{"type": "string"}'
